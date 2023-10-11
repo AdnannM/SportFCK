21133d1900f5447eb0f04cf4f8b4ae74e85f3a12
AdnannM <adnannmuratovic@icloud.com>
2023-10-11 14:41:19 +0200

    feat: add table view in ather two controllers

--- FC Kufstein/General/TaBarControllers/MatchController.swift
+++ FC Kufstein/General/TaBarControllers/MatchController.swift
@@ -9,10 +9,57 @@ import UIKit
 
 class MatchController: UIViewController {
     
+    // MARK: - Components
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+}
+
+// MARK: - Setup
+private extension MatchController {
+    private func setupUI() {
         view.backgroundColor = .systemGray5
         title = "Match"
+        
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+// MARK: - TableViewDelegate and TableViewDataSource
+extension MatchController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Testing ✅"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
