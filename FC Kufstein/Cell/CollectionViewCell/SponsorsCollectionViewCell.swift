a5fa40c06a208afc303c83aa2c45b38c7b80d2ef
AdnannM <adnannmuratovic@icloud.com>
2023-10-18 19:32:38 +0200

    refactor: clean some code

--- FC Kufstein/Cell/CollectionViewCell/SponsorsCollectionViewCell.swift
+++ FC Kufstein/Cell/CollectionViewCell/SponsorsCollectionViewCell.swift
@@ -7,25 +7,6 @@
 
 import UIKit
 
-struct SponsorsModelData {
-    let image: UIImage
-    let url: URL?
-}
-
-struct SponsorsData {
-    static let allSponsors: [SponsorsModelData] = [
-        SponsorsModelData(image: UIImage(named: "alpquell-logo")!, url: URL(string: "https://www.alpquell.com/")),
-        SponsorsModelData(image: UIImage(named: "FC Kufstein")!, url: URL(string: "https://www.kellys.at/")),
-        SponsorsModelData(image: UIImage(named: "stadt-kufstein")!, url: URL(string: "https://www.kufstein.gv.at/")),
-        SponsorsModelData(image: UIImage(named: "Interspar")!, url: URL(string: "https://www.interspar.at/")),
-        SponsorsModelData(image: UIImage(named: "kufnet")!, url: URL(string: "https://www.kufnet.at/")),
-        SponsorsModelData(image: UIImage(named: "kufstein-galerie")!, url: URL(string: "https://www.kufstein-galerien.at/home/?no_cache=1")),
-        SponsorsModelData(image: UIImage(named: "LOsteria1")!, url: URL(string: "https://losteria.net/at/")),
-        SponsorsModelData(image: UIImage(named: "moccador")!, url: URL(string: "https://www.moccador.at/de/partner-199/praesentation/aktuell")),
-        SponsorsModelData(image: UIImage(named: "rztrsdfdsdElement-1")!, url: URL(string: "https://www.ricado-networks.com/")),
-    ]
-}
-
 class SponsorsCollectionViewCell: UICollectionViewCell {
     
     // MARK: - Properties
