041690b2f30893fb38f9df1988a33ecc4173a57d
AdnannM <adnannmuratovic@icloud.com>
2023-10-28 21:11:28 +0200

    feat: adding shimmer efect to news cell before fetching the data

--- FC Kufstein/Cell/CollectionViewCell/ShimmerCollectionViewCell.swift
+++ FC Kufstein/Cell/CollectionViewCell/ShimmerCollectionViewCell.swift
@@ -9,91 +9,193 @@
 import UIKit
 import SkeletonView
 
-
 class ShimmerCollectionViewCell: UICollectionViewCell {
     
     static let cellID = "ShimmerCollectionViewCell"
     
-    private let contentContainerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .systemGray6
-        view.layer.cornerRadius = 20
-        view.layer.masksToBounds = true
-        return view
+    // MARK: - Components
+    private let containerView = createView(color: .systemGray6)
+    private let circleView = createView(color: .systemBackground)
+    private let shareButtonView = createView(color: .systemGray6)
+    
+    private let shareImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "square.and.arrow.up")
+        imageView.tintColor = .label // You can set the tint color
+        return imageView
     }()
     
-    private let stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.spacing = 8
-        return stackView
+    private let newsArticleImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .orange
+        let image = UIImage(named: "testImage")
+        imageView.image = image
+        return imageView
     }()
     
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .label
-        return label
-    }()
+    private let newsArticleTitle = UILabel.createCustomLabel(
+        text: "Der SK Ebbs siegte beim Qualifikationsturnier – unsere Mannschaft erreichte Platz 8",
+        textColor: .label,
+        fontSize: 17,
+        fontWeight: .medium
+    )
     
-    private let dateLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .secondaryLabel
-        return label
-    }()
+    private let newsDate = UILabel.createCustomLabel(
+        text: "09.10.2023",
+        textColor: .secondaryLabel,
+        fontSize: 14,
+        fontWeight: .regular
+    )
     
+    // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupUI()
         
-        contentView.addSubview(contentContainerView)
-        contentContainerView.addSubview(stackView)
-        stackView.addArrangedSubview(titleLabel)
-        stackView.addArrangedSubview(dateLabel)
-        
-        setupConstraints()
         setupShimmerEffect()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+// MARK: - SetupUI
+private extension ShimmerCollectionViewCell {
+    private func setupUI() {
+        setupContainerView()
+        setupCircleView()
+        setupShareButtonView()
+        setupShereImage()
+        setupNewsArticleImage()
+        setupNewsArticleTitle()
+    }
     
-    func setupConstraints() {
-        contentContainerView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+    private func setupContainerView() {
+        contentView.addSubview(containerView)
         
         NSLayoutConstraint.activate([
-            contentContainerView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            contentContainerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            contentContainerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            contentContainerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
+        
+        containerView.layer.cornerRadius = 20
+        containerView.layer.masksToBounds = true
+    }
+    
+    private func setupCircleView() {
+        containerView.addSubview(circleView)
+        
+        NSLayoutConstraint.activate([
+            circleView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 10),
+            circleView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: 5),
+            circleView.widthAnchor.constraint(equalToConstant: 80), // Set the width and height to create a circle
+            circleView.heightAnchor.constraint(equalTo: circleView.widthAnchor),
+        ])
+        
+        circleView.layer.cornerRadius = 40 // Set half of the width to make it circular
+        circleView.clipsToBounds = true
+    }
+    
+    private func setupShareButtonView() {
+        circleView.addSubview(shareButtonView)
+        
+        NSLayoutConstraint.activate([
+            shareButtonView.topAnchor.constraint(equalTo: circleView.topAnchor, constant: 10),
+            shareButtonView.trailingAnchor.constraint(equalTo: circleView.trailingAnchor, constant: -10),
+            shareButtonView.leadingAnchor.constraint(equalTo: circleView.leadingAnchor, constant: 10),
+            shareButtonView.bottomAnchor.constraint(equalTo: circleView.bottomAnchor, constant: -10),
             
-            stackView.topAnchor.constraint(equalTo: contentContainerView.topAnchor, constant: 16),
-            stackView.leadingAnchor.constraint(equalTo: contentContainerView.leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(equalTo: contentContainerView.trailingAnchor, constant: -16),
-            
-            titleLabel.heightAnchor.constraint(equalToConstant: 20),
-            dateLabel.heightAnchor.constraint(equalToConstant: 16),
+            shareButtonView.widthAnchor.constraint(equalToConstant: 60), // Set the width and height to create a circle
+            shareButtonView.heightAnchor.constraint(equalTo: shareButtonView.widthAnchor),
+        ])
+        
+        shareButtonView.clipsToBounds = true
+        shareButtonView.layer.cornerRadius = 30
+    }
+    
+    private func setupShereImage() {
+        shareButtonView.addSubview(shareImage)
+        
+        NSLayoutConstraint.activate([
+            shareImage.centerXAnchor.constraint(equalTo: shareButtonView.centerXAnchor),
+            shareImage.centerYAnchor.constraint(equalTo: shareButtonView.centerYAnchor),
+            shareImage.heightAnchor.constraint(equalToConstant: 30),
+            shareImage.widthAnchor.constraint(equalToConstant: 25)
         ])
     }
     
+    private func setupNewsArticleImage() {
+        containerView.addSubview(newsArticleImage)
+        newsArticleImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            newsArticleImage.topAnchor.constraint(equalTo: containerView.topAnchor),
+            newsArticleImage.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            newsArticleImage.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            newsArticleImage.heightAnchor.constraint(equalToConstant: 160)
+        ])
+        
+        newsArticleImage.layer.cornerRadius = 20
+        newsArticleImage.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+    }
+    
+    private func setupNewsArticleTitle() {
+        // Create a UIStackView to hold newsArticleTitle and newsDate
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 4 // Adjust the spacing as needed
+        stackView.alignment = .leading
+        
+        // Add newsArticleTitle and newsDate to the stackView
+        stackView.addArrangedSubview(newsArticleTitle)
+        stackView.addArrangedSubview(newsDate)
+        
+        // Add the stackView to containerView
+        containerView.addSubview(stackView)
+        
+        // Set maximum number of lines and add ellipsis for longer titles
+        newsArticleTitle.lineBreakMode = .byTruncatingTail
+        newsArticleTitle.numberOfLines = 2 // Adjust as needed based on your design
+        
+        NSLayoutConstraint.activate([
+            // Constraints for the stackView
+            stackView.topAnchor.constraint(equalTo: newsArticleImage.bottomAnchor, constant: 16),
+            stackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 12),
+            stackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -90),
+            
+        ])
+        newsArticleTitle.textAlignment = .left
+    }
+    
     func setupShimmerEffect() {
-        contentContainerView.isSkeletonable = true
-        contentContainerView.showAnimatedGradientSkeleton()
+        containerView.isSkeletonable = true
         
-        titleLabel.isSkeletonable = true
-        titleLabel.showAnimatedGradientSkeleton()
+        newsArticleTitle.isSkeletonable = true
+        newsArticleTitle.showAnimatedGradientSkeleton()
         
-        dateLabel.isSkeletonable = true
-        dateLabel.showAnimatedGradientSkeleton()
+        shareImage.isSkeletonable = true
+        shareImage.skeletonCornerRadius = 12
+        shareImage.showAnimatedGradientSkeleton()
+        
+        newsArticleImage.isSkeletonable = true
+        newsArticleImage.skeletonCornerRadius = 8
+        newsArticleImage.showAnimatedGradientSkeleton()
+        
+        newsDate.isSkeletonable = true
+        newsDate.showAnimatedGradientSkeleton()
     }
     
     func stopShimmer() {
-        contentContainerView.hideSkeleton()
-        titleLabel.hideSkeleton()
-        dateLabel.hideSkeleton()
+        containerView.hideSkeleton()
+        newsArticleTitle.hideSkeleton()
+        newsArticleImage.hideSkeleton()
+        newsDate.hideSkeleton()
     }
 }
-
-
